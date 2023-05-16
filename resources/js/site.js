0bc32ee93fa338420663024add226d72d98ecc8c
netnakgraham <graham@netnak.co.uk>
2023-05-16 16:39:00 +0100

    z

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,63 +1,72 @@
-// This is all you.
- // Load the seating data from the JSON file
- fetch('seating.json')
- .then(response => response.json())
- .then(data => {
-   const seatingPlanDiv = document.getElementById('seatingPlan');
-   
-   // Create the seating plan sections
-   data.sections.forEach(section => {
-	 const sectionDiv = document.createElement('div');
-	 sectionDiv.className = 'section';
-	 const sectionName = document.createElement('h2');
-	 sectionName.textContent = section.name;
-	 sectionDiv.appendChild(sectionName);
-	 
-	 // Create the seating plan rows within each section
-	 section.rows.forEach(row => {
-	   const rowDiv = document.createElement('div');
-	   rowDiv.className = 'row';
-	   const rowNumber = document.createElement('span');
-	   rowNumber.textContent = 'Row ' + row.number + ': ';
-	   rowDiv.appendChild(rowNumber);
-	   
-	   // Create the seats within each row
-	   row.seats.forEach(seat => {
-		 const seatDiv = document.createElement('div');
-		 seatDiv.className = 'seat ' + seat.status;
-		 seatDiv.textContent = seat.number;
-		 seatDiv.addEventListener('click', () => {
-		   if (seat.status === 'available') {
-			 // Temporarily reserve the seat
-			 seat.status = 'reserved';
-			 seatDiv.className = 'seat reserved';
-			 setTimeout(() => {
-			   // After a certain period, release the reservation
-			   seat.status = 'available';
-			   seatDiv.className = 'seat available';
-			 }, 5000); // 5 seconds
-			 // Send a reservation request to the server
-			 const xhr = new XMLHttpRequest();
-			 xhr.onreadystatechange = function() {
-			   if (xhr.readyState === 4 && xhr.status === 200) {
-				 console.log(xhr.responseText);
-			   }
-			 };
-			 xhr
+const shoppingCart = [];
 
-.open('POST', 'reserve-seat.php', true);
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-xhr.send('seat=' + encodeURIComponent(seat.number));
-}
-});
-rowDiv.appendChild(seatDiv);
-});
+// Load the seating data from the JSON file
+fetch('seating.json')
+  .then(response => response.json())
+  .then(data => {
+	const seatingPlanDiv = document.getElementById('seatingPlan');
+	const shoppingCartUl = document.getElementById('shoppingCart');
+
+	// Create the seating plan sections
+	data.sections.forEach(section => {
+	  const sectionDiv = document.createElement('div');
+	  sectionDiv.className = 'section';
+	  const sectionName = document.createElement('h2');
+	  sectionName.textContent = section.name;
+	  sectionDiv.appendChild(sectionName);
+
+	  // Create the seating plan rows within each section
+	  section.rows.forEach(row => {
+		const rowDiv = document.createElement('div');
+		rowDiv.className = 'row';
+		const rowNumber = document.createElement('span');
+		rowNumber.textContent = 'Row ' + row.number + ': ';
+		rowDiv.appendChild(rowNumber);
+
+		// Create the seats within each row
+		row.seats.forEach(seat => {
+		  const seatDiv = document.createElement('div');
+		  seatDiv.className = 'seat ' + seat.status;
+		  seatDiv.textContent = seat.number;
+		  seatDiv.addEventListener('click', () => {
+			if (seat.status === 'available') {
+			  // Temporarily reserve the seat
+			  seat.status = 'reserved';
+			  seatDiv.className = 'seat reserved';
+			  setTimeout(() => {
+				// After a certain period, release the reservation
 
 
 
-   sectionDiv.appendChild(rowDiv);
- });
- 
- seatingPlanDiv.appendChild(sectionDiv);
+			seat.status = 'available';
+			seatDiv.className = 'seat available';
+		  }, 5000); // 5 seconds
+
+		  // Add reserved seat ID to shopping cart
+		  shoppingCart.push(seat.number);
+		  const cartItem = document.createElement('li');
+		  cartItem.textContent = seat.number;
+		  shoppingCartUl.appendChild(cartItem);
+		}
+	  });
+	  rowDiv.appendChild(seatDiv);
+
+	  // Add aisle breaks
+
+	  console.log(section.aisleBreaks);
+	  console.log(seat.number);
+	  if (section.aisleBreaks.includes(seat.number)) {
+		console.log("yo");
+
+		const aisleBreak = document.createElement('span');
+		aisleBreak.textContent = '|  | ';
+		rowDiv.appendChild(aisleBreak);
+	  }
+	});
+
+	sectionDiv.appendChild(rowDiv);
+  });
+
+  seatingPlanDiv.appendChild(sectionDiv);
 });
 });
