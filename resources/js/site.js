7c05c9b14fa526d8fc17d774078750f741390e29
DESKTOP-2UOPF8E\Me <graham@netnak.co.uk>
2023-05-17 21:04:24 +0100

    Update site.js

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,115 +1,8 @@
-/*const shoppingCart = [];
-
-// Load the seating data from the JSON file
-fetch('seating.json')
-  .then(response => response.json())
-  .then(data => {
-	const seatingPlanDiv = document.getElementById('seatingPlan');
-	const shoppingCartUl = document.getElementById('shoppingCart');
-
-	// Create the seating plan sections
-	data.sections.forEach(section => {
-	  const sectionDiv = document.createElement('div');
-	  sectionDiv.className = 'section';
-	  const sectionName = document.createElement('h2');
-	  sectionName.textContent = section.name;
-	  sectionDiv.appendChild(sectionName);
-
-	  // Create the seating plan rows within each section
-	  section.rows.forEach(row => {
-		const rowDiv = document.createElement('div');
-		rowDiv.className = 'row';
-		const rowNumber = document.createElement('span');
-		rowNumber.textContent = 'Row ' + row.number + ': ';
-		rowDiv.appendChild(rowNumber);
-
-		// Create the seats within each row
-		row.seats.forEach(seat => {
-		  const seatDiv = document.createElement('div');
-		  seatDiv.className = 'seat ' + seat.status;
-		  seatDiv.textContent = seat.number;
-		  seatDiv.addEventListener('click', () => {
-			if (seat.status === 'available') {
-			  // Temporarily reserve the seat
-			  seat.status = 'reserved';
-			  seatDiv.className = 'seat reserved';
-			  setTimeout(() => {
-				// After a certain period, release the reservation
 
 
 
-			seat.status = 'available';
-			seatDiv.className = 'seat available';
-		  }, 5000); // 5 seconds
 
-		  // Add reserved seat ID to shopping cart
-		  shoppingCart.push(seat.number);
-		  const cartItem = document.createElement('li');
-		  cartItem.textContent = seat.number;
-		  shoppingCartUl.appendChild(cartItem);
-		}
-	  });
-	  rowDiv.appendChild(seatDiv);
 
-	  // Add aisle breaks
-
-	  console.log(section.aisleBreaks);
-	  console.log(seat.number);
-	  if (section.aisleBreaks.includes(seat.number)) {
-		console.log("yo");
-
-		const aisleBreak = document.createElement('span');
-		aisleBreak.textContent = '|  | ';
-		rowDiv.appendChild(aisleBreak);
-	  }
-	});
-
-	sectionDiv.appendChild(rowDiv);
-  });
-
-  seatingPlanDiv.appendChild(sectionDiv);
-});
-});*/
-
-// Your JSON seating plan
-var seatingPlanx = {
-    "venueName": "Example Theater",
-    "stage": {
-        "rows": 10,
-        "seatsPerRow": 12,
-    },
-	"sections": [
-		{
-		  "name": "Front",
-		  "position": "front",
-		  "rows": 10,
-		  "seatsPerRow": 10,
-		  "aisleBreaks": []
-		},
-		{
-		  "name": "Side Left",
-		  "position": "left",
-		  "rows": 5,
-		  "seatsPerRow": 5,
-		  "aisleBreaks": [3]
-		},
-		{
-		  "name": "Side Right",
-		  "position": "right",
-		  "rows": 5,
-		  "seatsPerRow": 5,
-		  "aisleBreaks": [3]
-		},
-		{
-		  "name": "Rear",
-		  "position": "rear",
-		  "rows": 8,
-		  "seatsPerRow": 12,
-		  "aisleBreaks": []
-		}
-	  ]
-};
- 
 const seatingPlan = [];
 
     interact('.section')
@@ -264,3 +157,119 @@ const seatingPlan = [];
       });
       return seatNumber;
     }
+
+
+
+
+/*const shoppingCart = [];
+
+// Load the seating data from the JSON file
+fetch('seating.json')
+  .then(response => response.json())
+  .then(data => {
+	const seatingPlanDiv = document.getElementById('seatingPlan');
+	const shoppingCartUl = document.getElementById('shoppingCart');
+
+	// Create the seating plan sections
+	data.sections.forEach(section => {
+	  const sectionDiv = document.createElement('div');
+	  sectionDiv.className = 'section';
+	  const sectionName = document.createElement('h2');
+	  sectionName.textContent = section.name;
+	  sectionDiv.appendChild(sectionName);
+
+	  // Create the seating plan rows within each section
+	  section.rows.forEach(row => {
+		const rowDiv = document.createElement('div');
+		rowDiv.className = 'row';
+		const rowNumber = document.createElement('span');
+		rowNumber.textContent = 'Row ' + row.number + ': ';
+		rowDiv.appendChild(rowNumber);
+
+		// Create the seats within each row
+		row.seats.forEach(seat => {
+		  const seatDiv = document.createElement('div');
+		  seatDiv.className = 'seat ' + seat.status;
+		  seatDiv.textContent = seat.number;
+		  seatDiv.addEventListener('click', () => {
+			if (seat.status === 'available') {
+			  // Temporarily reserve the seat
+			  seat.status = 'reserved';
+			  seatDiv.className = 'seat reserved';
+			  setTimeout(() => {
+				// After a certain period, release the reservation
+
+
+
+			seat.status = 'available';
+			seatDiv.className = 'seat available';
+		  }, 5000); // 5 seconds
+
+		  // Add reserved seat ID to shopping cart
+		  shoppingCart.push(seat.number);
+		  const cartItem = document.createElement('li');
+		  cartItem.textContent = seat.number;
+		  shoppingCartUl.appendChild(cartItem);
+		}
+	  });
+	  rowDiv.appendChild(seatDiv);
+
+	  // Add aisle breaks
+
+	  console.log(section.aisleBreaks);
+	  console.log(seat.number);
+	  if (section.aisleBreaks.includes(seat.number)) {
+		console.log("yo");
+
+		const aisleBreak = document.createElement('span');
+		aisleBreak.textContent = '|  | ';
+		rowDiv.appendChild(aisleBreak);
+	  }
+	});
+
+	sectionDiv.appendChild(rowDiv);
+  });
+
+  seatingPlanDiv.appendChild(sectionDiv);
+});
+});
+
+// Your JSON seating plan
+var seatingPlanx = {
+    "venueName": "Example Theater",
+    "stage": {
+        "rows": 10,
+        "seatsPerRow": 12,
+    },
+	"sections": [
+		{
+		  "name": "Front",
+		  "position": "front",
+		  "rows": 10,
+		  "seatsPerRow": 10,
+		  "aisleBreaks": []
+		},
+		{
+		  "name": "Side Left",
+		  "position": "left",
+		  "rows": 5,
+		  "seatsPerRow": 5,
+		  "aisleBreaks": [3]
+		},
+		{
+		  "name": "Side Right",
+		  "position": "right",
+		  "rows": 5,
+		  "seatsPerRow": 5,
+		  "aisleBreaks": [3]
+		},
+		{
+		  "name": "Rear",
+		  "position": "rear",
+		  "rows": 8,
+		  "seatsPerRow": 12,
+		  "aisleBreaks": []
+		}
+	  ]
+};*/
+ 
